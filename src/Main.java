153801eead5dd0171c50bc95dd4f6a0bc5d45d51
Zara721 <zaza275@icloud.com>
2024-02-10 19:29:09 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        //You should write the
+        //code to control the turns, keep track of overall scores, and determine an actual winner. Print
+        //appropriate information as the game progresses, including the total score lost (if lost), when
+        //doubles occur, whose turn it is, etc. Do not use any “global” variables, that is, variables declared
+        //in the spot where instance variables are declared in an ADT. This class should only need local
+        //variables.
+        //Handle inputs that may not be simply a ‘y’ or an ‘n’. When you read a line, and if the first char is
+        //a ‘y’, ‘Y’, ‘n’ or ‘N’ then continue and assume the appropriate response. Otherwise, ask the user
+        //for new input. For example, an entry of “Yo” is treated as “yes”, while an entry of “eyes” would
+        //ask the user for new input.
         System.out.println("Hello world!");
-        char[][] dice = {{' ', '_', '_', '_', '_', '_', '_', ' '},
+        char[][] dice = {{' ', '＿', '＿', '＿', '＿', ' ', ' ', ' '},
                 {'|', ' ', 'o', ' ', ' ', 'o', ' ', '|'},
                 {'|', ' ', 'o', ' ', ' ', 'o', ' ', '|'},
                 {'|', ' ', 'o', ' ', ' ', 'o', ' ', '|'},
-                {' ', '_', '_', '_', '_', '_', '_', ' '}};
-
-        String[][] dice2 = {{"   ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", "   "},
-                {" | ", "   ", " o ", "   ", "   ", " o ", "   ", " | "},
-                {" | ", "   ", "  ", "   ", "   ", "  ", "   ", " | "},
-                {" | ", "   ", " o ", "   ", "   ", " o ", "   ", " | "},
-                {"   ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ ", "   "}};
+                {' ', '‾', '‾', '‾', '‾', '‾', '‾', ' '}};
 
         printDice(dice);
     }
 
 
-
-    public static void printDice(char [][] diceArray) {
+    public static void printDice(char[][] diceArray) {
         for (int row = 0; row < diceArray.length; row++) {
             for (int col = 0; col < 8; col++) {
                 System.out.print(diceArray[row][col]);
---
+++ src/TwoDice.java
@@ -0,0 +1,35 @@
+public class TwoDice {
+    private char[][] dice1;
+    private char[][] dice2;
+
+    private int diceNum1;
+    private int diceNum2;
+
+    public void makeDieAsArray() {
+        //make two die from an array
+        //choose 2 random numbers
+        //store those numbers as two arrays
+    }
+
+    public String toString() {
+        //display the dice to standard output.
+        return null;
+    }
+
+    public void roll() {
+        //method to roll the dice
+    }
+
+    public void getValue() {
+        //return the total on the dice
+    }
+
+    public void isDoubles() {
+        //returns true exactly when the dice contains doubles and false otherwise.
+    }
+
+    public void hasSingleOne() {
+        //that returns true exactly when one of the dice has
+        //a one, but false when both dice have a one and false when neither dice has a one.
+    }
+}
